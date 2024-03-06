2b46251391b6db71fc2e3a4a5d6c6fcff4cc4b8f
webseed87 <thdwngusl@naver.com>
2024-03-06 13:29:38 +0900

    240306

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,9 @@
+// common.js 기능 작성 송주현
+// 한군데서만 쓰는 js는 해당 html 파일 안에 넣어놨으며 2군데 이상 쓰는 js는 common.js에 넣어놨습니다.
+// 체크 박스 전체 선택 전체 해제 기능
+// 이미지 + 버튼 클릭시 팝업으로 보여지는 이미지 팝업 배너 기능(이전 다음 버튼 포함)
+// 팝업 기능 부분 (AI견적 상세심도, 표준견적 견적정보)
+
 $(document).ready(function () {
     // 체크 박스 전체선택 전체 해제 부분
     $("#selectAllButton_list").on('click', function () {
@@ -11,7 +17,7 @@ $(document).ready(function () {
     // 이미지 팝업부분
     var currentIndex = 0;
     var $ul;
-
+    //모바일에서 팝업 이미지가 상단에 고정 되어 있는 부분 제어 모바일에서는 스크롤이 생기기 때문에 필요함
     $(".popup_warp").each(function () {
         var $popupWarp = $(this);
         $popupWarp.on('scroll', function () {
@@ -45,33 +51,35 @@ $(document).ready(function () {
             }
         });
     });
+    // 이벤트 버블링 안일어나게 체크박스 선택시 리턴
     $(".img_expand li").on('click', function (e) {
         if ($(e.target).is('input[type="checkbox"]')) {
             return;
         }
-
+        //클릭한 현재 값 받아오기
         var $clickedLi = $(this);
         $ul = $clickedLi.closest('.img_expand');
         var $images = $ul.find('li');
 
         currentIndex = $images.index($clickedLi);
         showImage();
+        //이미지가 2개보다 작으면 이전 다음 버튼 안보이게 
         if ($images.length < 2) {
             $(".img_popup .next_button").css('display', 'none');
             $(".img_popup .prev_button").css('display', 'none');
         }
     });
-
+    //다음 버튼 기능
     $(".img_popup .next_button").on('click', function () {
         currentIndex = (currentIndex + 1) % $ul.find('li').length;
         showImage();
     });
-
+    //이전 버튼 기능
     $(".img_popup .prev_button").on('click', function () {
         currentIndex = (currentIndex - 1 + $ul.find('li').length) % $ul.find('li').length;
         showImage();
     });
-
+    //현재 클릭한 이미지를 팝업으로 보여지게 하는 함수 부분
     function showImage() {
         var imageUrl = $ul.find('li').eq(currentIndex).find('img').attr('src');
         $(".img_popup_content").attr('src', imageUrl);
@@ -80,8 +88,8 @@ $(document).ready(function () {
         $(".img_popup_continer").css('display', 'block');
         $("body").addClass('popup_scroll');
     }
-
-    $(".img_close, .img_popup_continer").on('click', function () {
+    //닫기버튼(X) 버튼 누르거나 영역외 버튼 누르면 안보이게 하는 부분
+    $(".img_close").on('click', function () {
         $(".img_popup").css('display', 'none');
         $(".img_popup_continer").css('display', 'none');
         $("body").removeClass('popup_scroll');
@@ -89,13 +97,7 @@ $(document).ready(function () {
         $(".img_popup .prev_button").css('display', 'block');
     });
 
-    // 탭 메뉴 부분
-    $(".arrow_button").on("click", function () {
-        $(this).toggleClass("open");
-        $(this).closest(".depth_contents").find(".state").toggle();
-    });
-
-    // 상세 팝업 부분
+    // AI견적 상세심도, 표준견적 견적정보 팝업 부분
     $(".popup_button").on("click", function () {
         $(".popup_continer").css('display', 'block');
         $(".popup_warp").css('display', 'block');
@@ -107,24 +109,5 @@ $(document).ready(function () {
         $("body").removeClass('popup');
     })
 
-    //개인정보 토큰값에 따라 리스트 다르게 보이게
-    var userTypeSelect = $('#userType');
-    var table = $('.filter_list');
-    function toggleColumnVisibility(className, show) {
-        var columns = table.find('.' + className);
-        columns.css('display', show ? '' : 'none');
-    }
-    function handleUserTypeChange() {
-        var selectedValue = userTypeSelect.val();
-        if (selectedValue === 'agency') {
-            toggleColumnVisibility('ins_list', true);
-            toggleColumnVisibility('other_list', false);
-        } else {
-            toggleColumnVisibility('ins_list', false);
-            toggleColumnVisibility('other_list', true);
 
-        }
-    }
-    handleUserTypeChange();
-    userTypeSelect.on('change', handleUserTypeChange);
 })
--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,85 +1,72 @@
+// upload.js 기능 작성 송주현
+// 이미지 파일로 업로드, 드래그로 업로드 가능 모바일에서는 드래그로 업로드 기능 보이지 않음
+// 이미지 파일명 표시 이미지 각각 x버튼으로 삭제 가능
+// 전체 삭제 버튼으로 전체 삭제 기능
+// 드래그로 업로드시 파일을 드래그 안내 문구 안보였다가 파일이 하나도 없으면 다시 보이게 하는 기능
+
+
 document.addEventListener('DOMContentLoaded', function () {
-  let dropArea = document.getElementById("drop-area");
-let droppedFiles = [];
+//드롭 영역을 가져오고 드롭된 파일을 저장할 배열을 만듬
+let dropArea = document.getElementById("drop-area"); 
+let droppedFiles = []; 
 
-
+// 삭제 버튼 생성 후 삭제 버튼 클릭시 실행될 함수 선언
 let deleteAllButton = document.createElement('button');
 deleteAllButton.addEventListener('click', function () {
-  deleteAllImages();
+  deleteAllImages(); 
 });
 
-// Prevent default drag behaviors
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+//드래그 앤 드롭 이벤트의 기본 동작을 방지하기 위해 이벤트 리스너를 추가
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
   dropArea.addEventListener(eventName, preventDefaults, false)   
   document.body.addEventListener(eventName, preventDefaults, false)
-})
+});
 
-// Highlight drop area when item is dragged over it
-;['dragenter', 'dragover'].forEach(eventName => {
-  dropArea.addEventListener(eventName, highlight, false)
-})
-
-;['dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, unhighlight, false)
-})
-
-// Handle dropped files
+//드롭 이벤트를 처리하는 이벤트 리스너 추가
 dropArea.addEventListener('drop', handleDrop, false)
 
+//드래그 앤 드롭 이벤트의 기본 동작을 방지하는 함수
 function preventDefaults (e) {
-  e.preventDefault()
-  e.stopPropagation()
+  e.preventDefault() // 고유 동작을 중지 시키고 
+  e.stopPropagation() // 상위 엘리먼트로 이벤트 전파를 중단 시키기 위한 기본 함수
 }
 
-function highlight(e) {
-  dropArea.classList.add('highlight')
-}
-
-function unhighlight(e) {
-  dropArea.classList.remove('active')
-}
 dropArea.addEventListener('drop', handleDrop, false);
 
+// 드래그 시 실행시킬 함수
 function handleDrop(e) {
   var dt = e.dataTransfer;
   var files = dt.files;
-
-  // Reset droppedFiles array for each drop event
-  droppedFiles = [];
-
-  // Add dropped files to the array without sorting
-  droppedFiles = droppedFiles.concat([...files]);
-
+  droppedFiles = []; //드래그 한 파일을 배열에 담고 
+  droppedFiles = droppedFiles.concat([...files]); // 배열을 전개 연산자로 값을 풀기
   initializeProgress(droppedFiles.length);
-
-  // Process files in the order they were dropped
   processFilesSequentially(droppedFiles);
-
-  unhighlight();
 }
+// 삭제된 파일 배열을 재설정하고 각 파일의 진행 상태를 초기화
 function deleteAllImages() {
   let gallery = document.getElementById('gallery');
   let paragraphs = document.querySelectorAll('.my-form p');
   let deleteAllButton = document.getElementById('deleteAllButton');
 
+  // 갤러리 아래 모든 엘리먼트 삭제
   while (gallery.firstChild) {
     gallery.removeChild(gallery.firstChild);
   }
 
+  //삭제 버튼 누르면 다시 파일 드래그 안내 부분 보이게
   paragraphs.forEach(function (paragraph) {
     paragraph.classList.remove('hidden');
   });
 
-  // Remove the deleteAllButton after deleting all images
+  //전체 삭제 버튼을 눌렀을때 버튼 안보이게 
   if (deleteAllButton) {
     deleteAllButton.parentNode.removeChild(deleteAllButton);
   }
 
 }
+//파일을 순차적으로 처리하는 기능
 function processFilesSequentially(files) {
-  // Create a promise chain to process files in order
   let chain = Promise.resolve();
-
   files.forEach((file, index) => {
     chain = chain.then(() => {
       return new Promise((resolve) => {
@@ -90,36 +77,32 @@ function processFilesSequentially(files) {
     });
   });
 }
+
+// 파일 업로드버튼 눌렀을때 파일을 순차적으로 처리하는 기능
 let uploadProgress = []
-// let progressBar = document.getElementById('progress-bar')
-
 function initializeProgress(numFiles) {
-  // progressBar.value = 0
   uploadProgress = []
-
   for(let i = numFiles; i > 0; i--) {
     uploadProgress.push(0)
   }
 }
-
-function updateProgress(fileNumber, percent) {
-  uploadProgress[fileNumber] = percent
-  // let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
-  // progressBar.value = total
-}
+// 파일 입력 요소를 가져오고 파일 선택을 처리하기 위한 이벤트 리스너를 추가
 let fileInput = document.getElementById("fileElem");
 fileInput.addEventListener('change', function () {
   handleFiles(this.files);
 });
+// 선택한 파일을 처리하는 기능
 function handleFiles(files) {
   files = [...files];
 
+  // 각 파일의 진행 상황을 초기화하고 업로드/미리보기
   initializeProgress(files.length);
   files.forEach((file, index) => {
     uploadFile(file, index);
     previewFile(file);
   });
 }
+//파일 미리 보기 처리 하는 부분
 function previewFile(file) {
   let reader = new FileReader();
   reader.readAsDataURL(file);
@@ -129,30 +112,21 @@ function previewFile(file) {
     let fileNameContainer = document.createElement('div');
     let closeButton = document.createElement('span');
     var paragraphs = document.querySelectorAll('.my-form p');
-    // 로딩 이미지 추가 부분입니다, 이 부분을 리사이징 되면 삭제 하셔서 쓰세요
-    let loaderSVG = document.createElement('div');
-    loaderSVG.className = 'loader';
-    loaderSVG.innerHTML = `
-      <svg>
-        <defs>
-          <filter id="goo">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="2" result="blur" />
-            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 5 -2" result="gooey" />
-            <feComposite in="SourceGraphic" in2="gooey" operator="atop"/>
-          </filter>
-        </defs>
-      </svg>
-    `;
 
+    let loaderImgBox = document.createElement('div'); //로딩이미지 추가하는 div 
+    loaderImgBox.className = 'img-loader-box'; //로딩이미지 추가하는 div 클래스명 
+    
+    // 이미지 주소 속성
     img.src = reader.result;
-    closeButton.src = '/images/x.svg'; // Set the path to your close image
-    closeButton.className = 'close-image'; // Add a class for styling
+    closeButton.src = '/images/x.svg'; 
+    closeButton.className = 'close-image'; 
 
+    //파일 마다 삭제 버튼 관련된 이벤트 리스너 
     closeButton.addEventListener('click', function () {
       imgContainer.remove();
-      checkShowMessages(); // Check and show messages after removing the image
+      checkShowMessages(); 
 
-      // Check and remove the deleteAllButton if no images are left
+      //이미지가 하나도 없으면 전체 삭제 버튼 안보이게
       if (document.getElementById('gallery').childElementCount === 0) {
         let deleteAllButton = document.getElementById('deleteAllButton');
         if (deleteAllButton) {
@@ -164,17 +138,19 @@ function previewFile(file) {
     fileNameContainer.innerText = file.name;
     fileNameContainer.className = 'file-name';
 
+    //이미지 미리 보기에 넣을 엘리먼트
     imgContainer.appendChild(img);
-    imgContainer.appendChild(fileNameContainer); // Append file name container
+    imgContainer.appendChild(fileNameContainer); 
     imgContainer.appendChild(closeButton);
-   imgContainer.appendChild(loaderSVG); // 리사이징 완료 되면 삭제 해서 쓰세요.
+  //  imgContainer.appendChild(loaderImgBox); // 리사이징 완료 되면 삭제 해서 쓰세요.
 
+  //갤러리에 이미지 없으면 드래그 안내 문구 다시 띄우기
     document.getElementById('gallery').appendChild(imgContainer);
     paragraphs.forEach(function (paragraph) {
       paragraph.classList.add('hidden');
     });
 
-    // Create the deleteAllButton only if it doesn't exist
+    //전체 삭제 버튼이 없으면 다시 보여지는 부분 선언
     if (!document.getElementById('deleteAllButton')) {
       let deleteAllButton = document.createElement('button');
       deleteAllButton.id = 'deleteAllButton';
@@ -187,6 +163,7 @@ function previewFile(file) {
     }
   };
 }
+ //갤러리 아이디 부분에 이미지가 있는지 여부에 따라서 메세지 출력하는 함수
 function checkShowMessages() {
   let gallery = document.getElementById('gallery');
   let paragraphs = document.querySelectorAll('.my-form p');
@@ -196,38 +173,25 @@ function checkShowMessages() {
     paragraphs.forEach(function (paragraph) {
       paragraph.classList.remove('hidden');
     });
-
-    // Hide the deleteAllButton when there are no images
     if (deleteAllButton) {
       deleteAllButton.style.display = 'none';
     }
   } else {
-    // Show the deleteAllButton when there are images
     if (deleteAllButton) {
       deleteAllButton.style.display = 'block';
     }
   }
 }
+
+// XMLHttpRequest를 사용하여 Cloudinary 엔드포인트에 파일을 업로드하는 기능  
+// 참고문서 https://developer.mozilla.org/ko/docs/Web/API/XMLHttpRequest
+
 function uploadFile(file, i) {
-  var url = 'https://api.cloudinary.com/v1_1/joezimim007/image/upload'
+  var url = 'https://api.cloudinary.com/v1_1/urlservice/image/upload?api_key=842582577334323' // 통신 부분은 바꿔서 사용해주세요
   var xhr = new XMLHttpRequest()
   var formData = new FormData()
   xhr.open('POST', url, true)
   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-  
-  xhr.upload.addEventListener("progress", function(e) {
-    updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
-  })
-  
-  xhr.addEventListener('readystatechange', function(e) {
-    if (xhr.readyState == 4) {
-      if (xhr.status == 200) {
-        updateProgress(i, 100);
-      } else {
-        console.error('Error uploading file:', xhr.status, xhr.statusText);
-      }
-    }
-  })
   
   formData.append('upload_preset', 'ujpu6gyk');
   formData.append('file', file);
